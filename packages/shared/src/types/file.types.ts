42aef29012c814996a498a910a0fbf06cd62fa09
faliyaaakash <aakashfaliya1@gmail.com>
2026-08-16 14:52:09 +0530

    commit for update the file structure

---
+++ packages/shared/src/types/file.types.ts
@@ -0,0 +1,23 @@
+export interface FileMetadata{
+    fileId: string;
+  name: string;
+  size: number;
+  type: string;
+  totalChunks: number;
+  chunkSize: number;
+  salt: string; // Hex-encoded salt for PBKDF2
+}
+
+export interface ChunkHeader {
+  fileId: string;
+  chunkIndex: number;
+  byteOffset: number;
+  iv: string; // Hex-encoded 12-byte IV 
+  payloadLength: number;
+}
+
+export type FileTransferSignal =
+    { type: 'FILE_OFFER'; metadata: FileMetadata }
+  | { type: 'FILE_ACCEPT'; fileId: string; startByteOffset: number }
+  | { type: 'FILE_REJECT'; fileId: string; reason: string }
+  | { type: 'TRANSFER_COMPLETE'; fileId: string };
